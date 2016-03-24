4919fdfab24f45fc61ea0be05185a8f0c9676ba5
Ben Johnson <ben@gophronesis.com>
2016-03-24 18:03:34 -0400

    cleaning scrape-edgar-index.py

--- scrape-edgar-index.py
+++ scrape-edgar-index.py
@@ -1,13 +1,6 @@
-import re
-import os
-import sys
-import math
 import json
-import time
 import urllib2
 import argparse
-
-from pprint import pprint
 from datetime import datetime, date, timedelta
 from elasticsearch import Elasticsearch
 from elasticsearch.helpers import streaming_bulk
@@ -15,18 +8,19 @@ from elasticsearch.helpers import streaming_bulk
 # -- 
 # CLI
 
-parser = argparse.ArgumentParser(description = 'Scrape EDGAR indices')
-parser.add_argument('--from-scratch', dest = 'from_scratch', action = "store_true")
-parser.add_argument('--most-recent', dest = 'most_recent', action = "store_true")
-parser.add_argument("--config-path", type = str, action = 'store')
-
+parser = argparse.ArgumentParser(description='Scrape EDGAR indices')
+parser.add_argument('--from-scratch', dest='from_scratch', action="store_true")
+parser.add_argument('--min-year', type=int, dest='min_year', action="store", default=2011)
+parser.add_argument('--max-year', type=int, dest='max_year', action="store", default=int(date.today().year))
+parser.add_argument('--most-recent', dest='most_recent', action="store_true")
+parser.add_argument("--config-path", type=str, action='store')
 args = parser.parse_args()
 
 config = json.load(open(args.config_path))
 client = Elasticsearch([{"host" : config['es']['host'], "port" : config['es']['port']}])
 
 # -- 
-# update index with new docs
+# Functions
 
 def get_max_date():
     global config 
@@ -38,10 +32,9 @@ def get_max_date():
     d = client.search(index = config['edgar_index']['index'], body = query)
     return int(d['aggregations']['max']['value'])
 
-
 def download_index(yr, q, from_date = get_max_date()):
     global config
-    parsing  = False 
+    parsing = False 
     
     index_url = 'ftp://ftp.sec.gov/edgar/full-index/%d/QTR%d/master.idx' % (yr, q)
     for line in urllib2.urlopen(index_url):
@@ -63,13 +56,13 @@ def download_index(yr, q, from_date = get_max_date()):
                 }
             else: 
                 pass
-        if line[0] == '-':
+
+        elif line[0] == '-':
             parsing = True
 
 
 # -- 
-# run ingest
-
+# Run
 if args.most_recent:
     yr = date.today().year
     q  = date.today().month / 3 
@@ -77,9 +70,12 @@ if args.most_recent:
         print a
 
 elif args.from_scratch:
-    yrs  = range(2011, int(date.today().year))
+    yrs  = range(args.min_year, args.max_year)
     qtrs = [1, 2, 3, 4]
     for yr in yrs:
         for qtr in qtrs:
             for a, b in streaming_bulk(client, download_index(yr, q, from_date = -1), chunk_size = 1000):
                 print a
+
+else:
+    raise Exception('Specificy either `most_recent` or `from_scratch`')
