aaa21058679ae18585f3e257b3f9afaa4611017a
Emmett Culhane <culhane@SP2-16-242-ML.hsd1.pa.comcast.net>
2016-04-06 14:07:12 -0400

    command line args

--- cronjobs/run-symbology.sh
+++ cronjobs/run-symbology.sh
@@ -4,4 +4,4 @@ echo 'updating symbology index'
 
 path=/home/ubuntu/ernest
 
-/srv/software/spark-1.6.1/bin/spark-submit --jars /srv/software/spark-1.6.1/jars/elasticsearch-hadoop-2.2.0.jar $path/compute-symbology.py --most-recent --config-path='/home/ubuntu/ernest/config.json'
+/srv/software/spark-1.6.1/bin/spark-submit --jars /srv/software/spark-1.6.1/jars/elasticsearch-hadoop-2.2.0.jar $path/compute-symbology.py --last-week --config-path='/home/ubuntu/ernest/config.json'
