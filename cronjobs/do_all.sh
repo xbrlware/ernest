0d96579d11fa96b2be3b0abcb92671c3c019a1e5
Emmett Culhane <culhane@SP2-16-242-ML.hsd1.pa.comcast.net>
2016-04-06 13:49:59 -0400

    updating paths

--- cronjobs/do_all.sh
+++ cronjobs/do_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cd /home/ubuntu/ernest/cronjobs/ && ./run-edgar-index.sh
-cd /home/ubuntu/ernest/cronjobs/ && ./run-edgar-forms.sh
-cd /home/ubuntu/ernest/cronjobs/ && ./run-symbology.sh
-cd /home/ubuntu/ernest/cronjobs/ && ./run-ownership.sh
+cd /home/ubuntu/ernest/cronjobs/ && bash run-edgar-index.sh
+cd /home/ubuntu/ernest/cronjobs/ && bash run-edgar-forms.sh
+cd /home/ubuntu/ernest/cronjobs/ && bash run-symbology.sh
+cd /home/ubuntu/ernest/cronjobs/ && bash run-ownership.sh
 
