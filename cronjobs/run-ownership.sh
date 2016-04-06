c2b7dcd9efb9743e5182e9c9c6fce78d3e6a7160
Emmett Culhane <culhane@SP2-16-242-ML.hsd1.pa.comcast.net>
2016-04-06 14:02:22 -0400

    changing args for spark jobs in shell scripts

--- cronjobs/run-ownership.sh
+++ cronjobs/run-ownership.sh
@@ -4,4 +4,4 @@ echo 'updating ownership index'
 
 path=/home/ubuntu/ernest
 
-/srv/software/spark-1.6.1/bin/spark-submit --jars /srv/software/spark-1.6.1/jars/elasticsearch-hadoop-2.2.0.jar $path/compute-ownership-graph.py --from-scratch --config-path='/home/ubuntu/ernest/config.json'
+/srv/software/spark-1.6.1/bin/spark-submit --jars /srv/software/spark-1.6.1/jars/elasticsearch-hadoop-2.2.0.jar $path/compute-ownership-graph.py --most-recent --config-path='/home/ubuntu/ernest/config.json'
