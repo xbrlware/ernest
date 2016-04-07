393b100953236497346cfb9b9377a340e9d53615
Emmett Culhane <culhane@SP2-16-242-ML.hsd1.pa.comcast.net>
2016-04-07 16:35:58 -0400

    added investor forums submodule

--- cronjobs/do_all.sh
+++ cronjobs/do_all.sh
@@ -4,4 +4,4 @@ cd /home/ubuntu/ernest/cronjobs/ && bash run-edgar-index.sh
 cd /home/ubuntu/ernest/cronjobs/ && bash run-edgar-forms.sh
 cd /home/ubuntu/ernest/cronjobs/ && bash run-symbology.sh
 cd /home/ubuntu/ernest/cronjobs/ && bash run-ownership.sh
-
+cd /home/ubuntu/ernest/investor-forums/cronjobs/ && bash run-daily.sh 
