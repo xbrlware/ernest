84966fda466b4dca91108d6b7b03435a5e418fd9
MorganPeterson <morgan@gophronesis.com>
2016-04-29 20:34:11 +0000

    Commandline Args added; README updated

---
+++ trade_suspensions/README.md
@@ -0,0 +1,6 @@
+#SEC Suspensions Scraper
+
+## Usage
+```python
+$ ./SEC_Suspension_scrape.py --config-path config.json
+```
--- trade_suspensions/SEC_Suspension_scrape.py
+++ trade_suspensions/SEC_Suspension_scrape.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import argparse
+import json
 import pdfquery
 import re
 import sys
@@ -16,54 +18,47 @@ else:
 
 
 class ScrapeSEC:
-    def __init__(self):
+    def __init__(self, config):
         self.aka = re.compile('(\(*./k/a\)*) ([A-Za-z\.\, ]+)')
         self.btypes = re.compile(
-            '(.*)(Inc|INC|Llc|LLC|Comp|COMP|Ltd|LTD|Corp|CORP)(\.*)')
+            '(.*)(Inc|INC|Llc|LLC|Comp|COMP|Company|Ltd|LTD|Limited|Corp|CORP|Corporation|CORPORATION|Co|N\.V\.|Bancorp|et al|Group)(\.*)')
         self.date_rex = re.compile(
             '[JFMASOND][aepuco][nbrynlgptvc]\.{0,1} [0-3][0-9], 20[0-1][0-6]')
-        self.client = Elasticsearch(["localhost"], port=9205)
-        self.doc_type = 'suspension'
-        self.domain = "http://sec.gov"
-        self.es_index = 'ernest_suspensions'
-        self.first_year = 1994
-        self.main_sleep = 5
-        self.next_year = 2015
-        self.page_current = "http://www.sec.gov/litigation/suspensions.shtml"
-        self.scrape_sleep = 3
-        self.url_fmt = "http://www.sec.gov/litigation/suspensions/suspensionsarchive/susparch{}.shtml"
+        self.client = Elasticsearch([config['es']['host']], port=config['es']['port'])
+        self.doc_type = config['es']['doc']
+        self.domain = config['domain']
+        self.es_index = config['es']['index']
+        self.first_year = config['start_year']
+        self.main_sleep = config['sleep_time']
+        self.next_year = config['end_year']
+        self.page_current = config['home_url']
+        self.scrape_sleep = config['sleep_time']
+        self.url_fmt = config['url_fmt_str']
 
     def combine_date_links(self, dates, links):
-        """ combine url and date into one object """
         return [{"date": dates[i], "link": links[i]} for i in range(0, len(dates))]
 
     def es_ingest(self, j_obj):
-        """ ingest json object into an elasticsearch index """
         self.client.index(index=self.es_index, doc_type=self.doc_type, body=j_obj)
 
     def get_html(self, url):
-        """ get raw html from url """
         response = urlopen(url)
         return response.read()
 
     def get_pdf(self, pdf_link, pdf_out_loc):
-        """ load pdf from file and return release number"""
         response = urlopen(pdf_link)
         with open(pdf_out_loc, 'wb') as outf:
             outf.write(response.read())
-        return int(''.join(pdf_link.split('/')[-1:][0].split('-')[:-1]))
+        return '-'.join(pdf_link.split('/')[-1:][0].split('-')[:-1])
 
     def grab_dates(self, soup_object):
-        """ grab all dates from soup and format it before return in date"""
         d = [re.match(self.date_rex, ele.text).group(0) for ele in soup_object.findAll('td') if re.match(self.date_rex, ele.text)]
-        return [datetime.strptime(x.replace('.', '').replace(',', ''), "%b %d %Y").strftime('%m-%d-%y') for x in d]
+        return [datetime.strptime(x.replace('.', '').replace(',', ''), "%b %d %Y").strftime('%m-%d-%Y') for x in d]
 
     def grab_links(self, soup_obj, domain):
-        """ grab html page and get pdf links from it """
         return [domain + ele for ele in self.link_filter(soup_obj('a', href=True))]
 
     def link_filter(self, seq):
-        """ generator that takes a list of <a> and returns href """
         for ele in seq:
             if '-o.pdf' in ele['href']:
                 yield ele['href']
@@ -79,7 +74,6 @@ class ScrapeSEC:
             i -= 1
 
     def parse_pdf(self, pdf_location, xml_out_loc):
-        """ use pdfquery to parse pdf and write out to xml """
         pdf = pdfquery.PDFQuery(pdf_location,
                                 merge_tags=('LTChar'),
                                 round_floats=True,
@@ -93,14 +87,12 @@ class ScrapeSEC:
 
         pdf.load()
         pdf.tree.write(xml_out_loc)
-        return True
 
     def parse_xml(self, xml_loc):
-        """ parse out business names from XML file """
         i = 0
         company_list = []
-        c = {"str": "", "flag": False}  # cache dictionary for 2 line names
-        soup = self.xml_to_soup(xml_loc)  # convert xml to BS object
+        c = {"str": "", "flag": False}
+        soup = self.xml_to_soup(xml_loc)
 
         for ele in soup.findAll('LTTextLineHorizontal'):
             m = re.search(self.btypes, ele.text)
@@ -135,7 +127,6 @@ class ScrapeSEC:
         return company_list
 
     def scrape(self, get_page):
-        """ the full scrape of a page """
         domain = self.domain
         page_text = self.get_html(get_page)
         soup = BeautifulSoup(page_text, 'xml')
@@ -150,10 +141,10 @@ class ScrapeSEC:
             c_list = self.parse_xml('/tmp/sec_temp_file.xml')
             for c in c_list:
                 self.es_ingest({"release_number": release, "link": l['link'], "date": l['date'], "company": c})
+                print({"release_number": release, "link": l['link'], "date": l['date'], "company": c})
             time.sleep(self.scrape_sleep)
 
     def xml_to_soup(self, xml_loc):
-        """ convert xml file into soup object """
         with open('/tmp/sec_temp_file.xml', 'r') as inf:
             x = inf.read()
 
@@ -161,4 +152,10 @@ class ScrapeSEC:
         return soup
 
 if __name__ == "__main__":
-    ScrapeSEC().main()
+    parser = argparse.ArgumentParser(description='scrape_trade_suspensions')
+    parser.add_argument("--config-path", type=str, action='store')
+    args = parser.parse_args()
+
+    config = json.load(open(args.config_path))
+
+    ScrapeSEC(config).main()
