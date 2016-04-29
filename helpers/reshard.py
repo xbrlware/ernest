49adafef7fd1d5d5fc73de0fcdae0c90b876bc6c
Ben Johnson <ben@gophronesis.com>
2016-04-29 01:36:58 -0400

    adding helpers

---
+++ helpers/reshard.py
@@ -0,0 +1,25 @@
+import json
+from elasticsearch import Elasticsearch
+from elasticsearch.helpers import reindex
+
+config = json.load(open('../config.json'))
+
+client = Elasticsearch([{
+    "host" : config['es']['host'],
+    "port" : config['es']['port'],   
+}])
+
+old_index = config['forms']['index']
+doc_type  = config['forms']['_type']
+new_index = old_index + '_reshard'
+
+# --
+
+client.indices.create(index=new_index, body={
+    "settings" : {
+        "number_of_shards"   : 20,
+        "number_of_replicas" : 0
+    }
+})
+
+reindex(client, index, new_index)
