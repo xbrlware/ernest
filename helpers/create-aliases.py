d41f3ba1c18b6d9349f5ee5515cd2e239a606452
Ben Johnson <ben@gophronesis.com>
2016-04-29 19:21:48 -0400

    aliases

--- helpers/create-aliases.py
+++ helpers/create-aliases.py
@@ -20,6 +20,5 @@ client = Elasticsearch([{
     "port" : config['es']['port']
 }])
 
-print '-- trying to add alias --'
+print 'adding alias \t\t %s \t\t -> \t\t %s' % (args.alias, args.index)
 client.indices.put_alias(index=args.index, name=args.alias)
-print '-- done --'
