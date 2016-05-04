05cd878f0fc979c6e93b243ff7142a76d9e8c112
ben <ben@gophronesis.com>
2016-05-04 04:56:39 +0000

    finishing aggregate-symbology

--- aggs/aggregate-symbology.py
+++ aggs/aggregate-symbology.py
@@ -4,15 +4,20 @@
 
 import json
 import argparse
+import itertools
+from collections import OrderedDict
+
+from pyspark import SparkContext
+sc = SparkContext(appName='aggregate-symbology.py')
 
 # -- 
 # CLI
 
-parser = argparse.ArgumentParser(description='grab_new_filings')
+parser = argparse.ArgumentParser(description='aggregate-symbology')
 parser.add_argument("--config-path", type=str, action='store')
-args   = parser.parse_args()
+args = parser.parse_args()
 
-config = json.load(open('/home/ubuntu/ernest/config.json'))
+config = json.load(open(args.config_path))
 
 # --
 # Connections
@@ -31,14 +36,23 @@ rdd = sc.newAPIHadoopRDD(
 # --
 # Functions
 
-def _changes(z, field):
-    
-
+def _changes(records, field):
+    old_record = records[0]
+    for new_record in records[1:]:
+        if new_record[field] != old_record[field]:
+            yield OrderedDict([
+                ( "field"    , field ),
+                ( "old_val"  , old_record[field] ),
+                ( "new_val"  , new_record[field] ),
+                ( "old_date" , old_record['max_date'] ),
+                ( "new_date" , new_record['min_date'] ),
+            ])
+        
+        old_record = new_record    
 
 def all_changes(x):
-    z = x[1]
-    z = sorted(z, key=lambda x: x['min_date'])
-    return _changes(z, 'name') + _changes(z, 'sic') + _changes(z, 'ticker')
+    x = sorted(x, key=lambda x: x['min_date'])
+    return itertools.chain(*[_changes(x, field) for field in ['name', 'sic', 'ticker']])
 
 # --
 # Run
@@ -46,4 +60,21 @@ def all_changes(x):
 rdd.map(lambda x: (x[1]['cik'], x[1]))\
     .groupByKey()\
     .mapValues(all_changes)\
-    .take(10)
+    .map(lambda x: ('-', {"cik" : x[0], "symbology" : tuple(x[1])}))\
+    .filter(lambda x: len(x[1]['symbology']) > 0)\
+    .mapValues(json.dumps)\
+    .saveAsNewAPIHadoopFile(
+        path = '-',
+        outputFormatClass = 'org.elasticsearch.hadoop.mr.EsOutputFormat',
+        keyClass = 'org.apache.hadoop.io.NullWritable', 
+        valueClass = 'org.elasticsearch.hadoop.mr.LinkedMapWritable', 
+        conf = {
+            'es.input.json'      : 'true',
+            'es.nodes'           : config['es']['host'],
+            'es.port'            : str(config['es']['port']),
+            'es.resource'        : '%s/%s' % (config['agg']['index'], config['agg']['_type']),
+            'es.mapping.id'      : 'cik',
+            'es.write.operation' : 'upsert'
+        }
+    )
+
