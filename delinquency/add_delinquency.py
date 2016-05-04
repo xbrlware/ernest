b1bbc5400928a1a7c47651019fc3414658186a72
Ben Johnson <ben@gophronesis.com>
2016-05-04 19:37:22 -0400

    fixing add_delinquency

--- delinquency/add_delinquency.py
+++ delinquency/add_delinquency.py
@@ -5,10 +5,25 @@ import holidays
 import dateutil.parser as parser
 
 import datetime
-from datetime import date, timedelta
+from datetime import timedelta
 
 from elasticsearch import Elasticsearch
 from elasticsearch.helpers import scan, streaming_bulk
+
+deadlines = {
+    "10-K" : {
+        "Large Accelerated Filer"   : 60,
+        "Accelerated Filer"         : 75,
+        "Non-accelerated Filer"     : 90,
+        "Smaller Reporting Company" : 90
+    }
+    "10-Q" : {
+        "Large Accelerated Filer"   : 40,
+        "Accelerated Filer"         : 40,
+        "Non-accelerated Filer"     : 45,
+        "Smaller Reporting Company" : 45
+    }
+}
 
 # -- 
 # CLI
@@ -51,110 +66,31 @@ query = {
 # --
 # Functions
 
-def add_delinquency(doc): 
-    doc = doc['_source']
-    if doc['form'] == "10-K": 
-        doc = __flag_10K(doc)
-    elif doc['form'] == "10-Q": 
-        doc = __flag_10Q(doc)
-    
-    return doc
-
-
-def __flag_10K(doc): 
-    r = [int(i) for i in doc['_enrich']['period'].split('-')]
+def add_delinquency(src, us_holidays=holidays.US()): 
+    r = map(int, src['_enrich']['period'].split('-'))
     d = datetime.date(r[0], r[1], r[2])  
     
-    if doc['_enrich']['status'] == 'Large Accelerated Filer': 
-        dl = __find_day(d + timedelta(days = 60))
-        doc['_enrich']['deadline'] = dl
+    dl = d + timedelta(days=deadlines[mode][src['form']['_enrich']['status']])
+    while (dl in us_holidays) or (dl.weekday >= 5):
+        dl += timedelta(days=1)
     
-    elif doc['_enrich']['status'] == 'Accelerated Filer': 
-        dl = __find_day(d + timedelta(days = 75))
-        doc['_enrich']['deadline'] = dl
-    
-    elif doc['_enrich']['status'] in ('Non-accelerated Filer', 'Smaller Reporting Company'): 
-        dl = __find_day(d + timedelta(days = 90))
-        doc['_enrich']['deadline'] = dl
-    
-    return __is_late(doc)
-  
-
-def __flag_10Q(doc): 
-    r = [int(i) for i in doc['_enrich']['period'].split('-')]
-    d = datetime.date(r[0], r[1], r[2])  
-    if doc['_enrich']['status'] in ('Large Accelerated Filer', 'Accelerated Filer'): 
-        dl = __find_day(d + timedelta(days = 40))
-        doc['_enrich']['deadline'] = dl
-        
-    elif doc['_enrich']['status'] in ('Non-accelerated Filer', 'Smaller Reporting Company'): 
-        dl = __find_day(d + timedelta(days = 45))
-        doc['_enrich']['deadline'] = dl
-    
-    return __is_late(doc)
-
-
-def __find_day(dl):
-    us_holidays = holidays.US()
-    if (dl.weekday() < 5) and (dl not in us_holidays): 
-        dl = dl
-        
-    elif dl.weekday() == 5: 
-        dl = dl + timedelta(days = 2)
-        if dl in us_holidays: 
-            dl = dl + timedelta(days = 1)
-        else: 
-            dl = dl
-            
-    elif dl.weekday() == 6: 
-        dl = dl + timedelta(days = 1)
-        if dl in us_holidays: 
-            dl = dl + timedelta(days = 1)
-        else: 
-            dl = dl
-            
-    elif dl in us_holidays: 
-        dl = dl + timedelta(days = 1)
-        if dl.weekday() == 5: 
-            dl = dl + timedelta(days = 2)
-            if dl in us_holidays: 
-                dl = dl + timedelta(days = 1)
-            else: 
-                dl = dl
-        
-        if dl.weekday() == 6: 
-            l = dl + timedelta(days = 1)
-            if dl in us_holidays: 
-                dl = dl + timedelta(days = 1)
-            else: 
-                dl = dl
-            
-    return dl
-
-
-def __is_late(doc): 
-    filed = [int(i) for i in doc['date'].split('-')]
+    filed = map(int, src['date'].split('-'))
     filed = datetime.date(filed[0], filed[1], filed[2])  
-    doc['_enrich']['days_to_deadline'] = (doc['_enrich']['deadline'] - filed).days
-    
-    doc['_enrich']['deadline'] = doc['_enrich']['deadline'].strftime("%Y-%m-%d")
-    
-    if doc['_enrich']['days_to_deadline'] < 0: 
-        doc['_enrich']['is_late'] = True
-    else: 
-        doc['_enrich']['is_late'] = False
-    
-    return doc 
+    src['_enrich']['deadline']         = dl.strftime("%Y-%m-%d")    
+    src['_enrich']['days_to_deadline'] = (dl - filed).days
+    src['_enrich']['is_late']          = src['_enrich']['days_to_deadline'] < 0
+
+    return src
 
 
 # --
 # Run
 
-for a in scan(client, index = config['delinquency']['index'], query = query): 
+for doc in scan(client, index = config['delinquency']['index'], query = query): 
     client.index(
         index    = config['delinquency']['index'], 
         doc_type = config['delinquency']['_type'], 
-        body     = add_delinquency(a), 
-        id       = a["_id"]
+        id       = doc["_id"],
+        body     = add_delinquency(doc['_source']), 
     )
 
