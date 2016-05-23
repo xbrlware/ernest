7eaf71cc3e6b80ad89f7d0d0d52c2d90735c966e
Emmett-Culhane <culhane@SP2-16-242-ML.hsd1.pa.comcast.net>
2016-05-23 17:27:17 -0400

    adding verbosity to finra shell script

--- cronjobs/run-finra-scrapes.sh
+++ cronjobs/run-finra-scrapes.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 echo "run-finra-scrapes.sh"
+echo "directory"
 python ../scrape/scrape-otc.py --directory='otc'
+echo "halts"
 python ../scrape/scrape-otc.py --directory='halts'
+echo "delinquency"
 python ../scrape/scrape-otc.py --directory='delinquency'
