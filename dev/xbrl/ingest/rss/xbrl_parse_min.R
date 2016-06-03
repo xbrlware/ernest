4d1e1c74bd083f65489a740878ebf270f4060e6e
Emmett-Culhane <culhane@SP2-16-242-ML.hsd1.pa.comcast.net>
2016-06-03 15:54:37 -0400

    debugging subprocess

--- dev/xbrl/ingest/rss/xbrl_parse_min.R
+++ dev/xbrl/ingest/rss/xbrl_parse_min.R
@@ -11,6 +11,7 @@ newdir <- paste('/home/ubuntu/xbrl/', args[1], '/', args[2], sep='')
 
 unzippedFiles<-list.files(newdir)
 finalDir     <-file.path(newdir,'parsed_min')
+print(finalDir)
 dir.create(finalDir, showWarnings = FALSE) 
 
 
@@ -29,7 +30,7 @@ for(u in unzippedFiles){
                 if(length(grep(pattern="[[:digit:]].xml", x=m))==1) { 
                     print(m) 
                     inst      <- file.path(newdir, u, m)
-                    xbrl.vars <- xbrlDoAll(inst, verbose=TRUE)
+                    xbrl.vars <- xbrlDoAll(inst, verbose=FALSE)
                     
                     # build frames
                     fact    <- buildFrame('fact')
@@ -37,8 +38,11 @@ for(u in unzippedFiles){
                     # joins tables to fact 
                     join1   <- merge(x = fact, y = context, by = "contextId", all.x = TRUE)
                     # write out file          
-                    title   <-gsub("-|.xml", "", u)  
-                    write.table(out, file = file.path(finalDir,paste0(title,'.csv')), sep = "," , append = TRUE)    
+                    title   <-gsub("-|.xml", "", m)  
+                    print(title)
+                    loc <- file.path(finalDir,paste0(title,'.csv'))
+                    print(loc) 
+                    write.table(out, file = loc, sep = "," , append = TRUE)    
                     
                 }
             }
