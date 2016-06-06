b897e3a101bd522a95da3a78700c1b41e67d5af4
Ben Johnson <ben@gophronesis.com>
2016-06-06 16:05:19 -0400

    fixing typo

--- cronjobs/run-add-otc-flag.py
+++ cronjobs/run-add-otc-flag.py
@@ -2,5 +2,5 @@
 
 echo "running enrich-add-otc-flag.py"
 
-python ../enrich/enrich-add-otc-flag.py --index ownership --field-name issuerName
+python ../enrich/enrich-add-otc-flag.py --index ownership --field-name issuerTradingSymbol
 python ../enrich/enrich-add-otc-flag.py --index symbology --field-name ticker
