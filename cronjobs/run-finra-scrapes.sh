9396e48e3aa5106205192725c09ed6dae999d10d
Ben Johnson <ben@gophronesis.com>
2016-06-06 15:34:01 -0400

    cleaning scrape-finra-directories.py

--- cronjobs/run-finra-scrapes.sh
+++ cronjobs/run-finra-scrapes.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 echo "run-finra-scrapes.sh"
+
 echo "directory"
-python ../scrape/scrape-finra-directories.py --directory='otc'
+python ../scrape/scrape-finra-directories.py --directory='directory'
+
 echo "halts"
 python ../scrape/scrape-finra-directories.py --directory='halts'
+
 echo "delinquency"
 python ../scrape/scrape-finra-directories.py --directory='delinquency'
