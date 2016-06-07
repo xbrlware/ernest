e40e1a0e96d46623fbe8405cab95f775ae690ef6
Emmett-Culhane <culhane@SP2-16-242-ML.hsd1.pa.comcast.net>
2016-06-07 12:11:56 -0400

    adding unified scrape for halts

--- scrape/scrape-finra-halts.py
+++ scrape/scrape-finra-halts.py
@@ -75,114 +75,108 @@ def build_date( date ):
 
 counter = 0
 while True:
-    try: 
-        time.sleep(6)
-        posts = BeautifulSoup(driver.page_source).findAll("tr", {'class' : ['odd', 'even']})  
-        time.sleeo(3)
-        for post in posts: 
-            facts = post.findAll('td')
-            time.sleep(3)
-            out   = {
-                'dateTime'      : build_date(facts[0].get_text()),
-                'ticker'        : facts[1].get_text().upper(), 
-                'issuerName'    : facts[2].get_text().upper(),
-                'haltCode'      : facts[3].get_text(),
-                'mktCtrOrigin'  : facts[4].get_text(),
-                'Action'        : facts[5].get_text(),
-                'secHalt'       : sec_halt(facts)
-            }
-            try: 
-                if out['secHalt'] == True and out['Action'] == 'halt': 
-                    res = client.search(index = INDEX, body = {
-                        "_source" : ["company", "date", "link"],
-                        "sort" : [
-                            {"_score" : {"order" : "desc"}}
-                        ],
-                        "query" : {
-                            "bool" : { 
-                                "must" : [
-                                {
-                                    "match" : {
-                                        "company" : out['issuerName']
-                                        }
-                                },
-                                {
-                                    "match" : { 
-                                        "date" : out['dateTime']
-                                    }
+    time.sleep(6)
+    posts = BeautifulSoup(driver.page_source).findAll("tr", {'class' : ['odd', 'even']})  
+    time.sleeo(3)
+    for post in posts: 
+        facts = post.findAll('td')
+        time.sleep(3)
+        out   = {
+            'dateTime'      : build_date(facts[0].get_text()),
+            'ticker'        : facts[1].get_text().upper(), 
+            'issuerName'    : facts[2].get_text().upper(),
+            'haltCode'      : facts[3].get_text(),
+            'mktCtrOrigin'  : facts[4].get_text(),
+            'Action'        : facts[5].get_text(),
+            'secHalt'       : sec_halt(facts)
+        }
+        if out['secHalt'] == True and out['Action'] == 'halt': 
+            res = client.search(index = INDEX, body = {
+                "_source" : ["company", "date", "link"],
+                "sort" : [
+                    {"_score" : {"order" : "desc"}}
+                ],
+                "query" : {
+                    "bool" : { 
+                        "must" : [
+                        {
+                            "match" : {
+                                "company" : out['issuerName']
                                 }
-                                    ]
-                                }
-                            }
-                        })
-                    if res['hits']['total'] > 0: 
-                        hit = res['hits']['hits'][0]['_source']
-                        _id = res['hits']['hits'][0]['_id']
-                        doc = { 
-                            'finra' : { 
-                                'ticker'    : out['ticker'],
-                                'halt_code' : out['haltCode'],
-                                'market'    : out['mktCtrOrigin'],
-                                'score'     : res['hits']['hits'][0]['_score'],
-                                'secHalt'   : True,
-                                'matched'   : True
+                        },
+                        {
+                            "match" : { 
+                                "date" : out['dateTime']
                             }
                         }
-                        hit['__meta__'] = doc
-                        client.index(index = INDEX, doc_type = TYPE, id = _id, body = hit)
-                    else: 
-                        _id = out['date'] + '_' + out['ticker']
-                        doc = { 
-                            "date"           : out['dateTime'],
-                            "company"        : out['issuerName'],
-                            "link"           : None,
-                            "release_number" : None,
-                            '__meta__' : { 
-                                'finra' : { 
-                                    'ticker'    : out['ticker'],
-                                    'halt_code' : out['haltCode'],
-                                    'market'    : out['mktCtrOrigin'],
-                                    'score'     : 0,
-                                    'secHalt'   : True,
-                                    'matched'   : False
-                                }
-                            }
+                            ]
                         }
-                        client.index(index = INDEX, doc_type = TYPE, id = _id, body = doc)
-                elif out['secHalt'] == False and out['Action'] == 'halt': 
-                    _id = out['date'] + '_' + out['ticker']
-                    doc = { 
-                        "date"           : out['dateTime'],
-                        "company"        : out['issuerName'],
-                        "link"           : None,
-                        "release_number" : None,
-                        '__meta__' : { 
-                            'finra' : { 
-                                'ticker'    : out['ticker'],
-                                'halt_code' : out['haltCode'],
-                                'market'    : out['mktCtrOrigin'],
-                                'score'     : 0,
-                                'secHalt'   : False,
-                                'matched'   : False
-                            }
+                    }
+                })
+            if res['hits']['total'] > 0: 
+                hit = res['hits']['hits'][0]['_source']
+                _id = res['hits']['hits'][0]['_id']
+                doc = { 
+                    'finra' : { 
+                        'ticker'    : out['ticker'],
+                        'halt_code' : out['haltCode'],
+                        'market'    : out['mktCtrOrigin'],
+                        'score'     : res['hits']['hits'][0]['_score'],
+                        'secHalt'   : True,
+                        'matched'   : True
+                    }
+                }
+                hit['__meta__'] = doc
+                client.index(index = INDEX, doc_type = TYPE, id = _id, body = hit)
+            else: 
+                _id = out['date'] + '_' + out['ticker']
+                doc = { 
+                    "date"           : out['dateTime'],
+                    "company"        : out['issuerName'],
+                    "link"           : None,
+                    "release_number" : None,
+                    '__meta__' : { 
+                        'finra' : { 
+                            'ticker'    : out['ticker'],
+                            'halt_code' : out['haltCode'],
+                            'market'    : out['mktCtrOrigin'],
+                            'score'     : 0,
+                            'secHalt'   : True,
+                            'matched'   : False
                         }
-                    }    
-                    client.index(index = INDEX, doc_type = TYPE, id = _id, body = doc)
-                else: 
-                    pass
-            except: 
-                print('ingest doc failed')
-                driver.quit()
-        counter += 1
-        print(counter)
+                    }
+                }
+                client.index(index = INDEX, doc_type = TYPE, id = _id, body = doc)
+        elif out['secHalt'] == False and out['Action'] == 'halt': 
+            _id = out['date'] + '_' + out['ticker']
+            doc = { 
+                "date"           : out['dateTime'],
+                "company"        : out['issuerName'],
+                "link"           : None,
+                "release_number" : None,
+                '__meta__' : { 
+                    'finra' : { 
+                        'ticker'    : out['ticker'],
+                        'halt_code' : out['haltCode'],
+                        'market'    : out['mktCtrOrigin'],
+                        'score'     : 0,
+                        'secHalt'   : False,
+                        'matched'   : False
+                    }
+                }
+            }    
+            client.index(index = INDEX, doc_type = TYPE, id = _id, body = doc)
+        else: 
+            pass
 
-        try:
-            driver.find_element_by_xpath("//*[contains(text(), 'Next')]").click()
-        except:
-            break
-    except: 
-        print('doc ingest failed')
-        driver.quit()
+    counter += 1
+    print(counter)
+
+    try:
+        driver.find_element_by_xpath("//*[contains(text(), 'Next')]").click()
+    except:
+        break
+
 
 driver.quit()
 
