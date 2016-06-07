c4c5520391c4c18b139ae0c6c919a147aa32a5aa
Emmett-Culhane <culhane@SP2-16-242-ML.hsd1.pa.comcast.net>
2016-06-07 13:07:13 -0400

    more debugging

--- scrape/scrape-finra-halts.py
+++ scrape/scrape-finra-halts.py
@@ -70,29 +70,33 @@ def build_date( date ):
 
 counter = 0
 while True:
-    time.sleep(6)
-    posts = BeautifulSoup(driver.page_source).findAll("tr", {'class' : ['odd', 'even']})  
-    time.sleeo(3)
-    for post in posts: 
-        facts = post.findAll('td')
+    try: 
+        time.sleep(6)
+        posts = BeautifulSoup(driver.page_source).findAll("tr", {'class' : ['odd', 'even']})  
         time.sleep(3)
-        out   = {
-            'dateTime'      : build_date( facts[0].get_text() ),
-            'ticker'        : facts[1].get_text().upper(), 
-            'issuerName'    : facts[2].get_text().upper(),
-            'haltCode'      : facts[3].get_text(),
-            'mktCtrOrigin'  : facts[4].get_text(),
-            'Action'        : facts[5].get_text(),
-            'secHalt'       : sec_halt(facts)
-        }
-        client.index(index=INDEX, doc_type=TYPE, body=out, id=out['dateTime'] + '_' + out['ticker']) 
+        for post in posts: 
+            facts = post.findAll('td')
+            time.sleep(3)
+            out   = {
+                'dateTime'      : build_date( facts[0].get_text() ),
+                'ticker'        : facts[1].get_text().upper(), 
+                'issuerName'    : facts[2].get_text().upper(),
+                'haltCode'      : facts[3].get_text(),
+                'mktCtrOrigin'  : facts[4].get_text(),
+                'Action'        : facts[5].get_text(),
+                'secHalt'       : sec_halt(facts)
+            }
+            client.index(index=INDEX, doc_type=TYPE, body=out, id=out['dateTime'] + '_' + out['ticker']) 
+            
+        counter += 1
+        print(counter)
+
+        try:
+            driver.find_element_by_xpath("//*[contains(text(), 'Next')]").click()
+        except:
+            break
+    except: 
+        driver.quit() 
+        raise
         
-    counter += 1
-    print(counter)
-
-    try:
-        driver.find_element_by_xpath("//*[contains(text(), 'Next')]").click()
-    except:
-        break
-
 driver.quit()
