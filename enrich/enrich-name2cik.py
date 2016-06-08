55fa6023970b40cc25e59e263cfb1478739bf3ff
ben <ben@gophronesis.com>
2016-06-08 13:43:58 +0000

    finishing name2cik

--- enrich/enrich-name2cik.py
+++ enrich/enrich-name2cik.py
@@ -1,100 +1,92 @@
+'''
+    Matches names to CIK numbers via Elasticsearch query
+'''
+
+import sys
 import json
 import argparse
 from datetime import datetime, date
-from fuzzywuzzy import fuzz, process
 from elasticsearch import Elasticsearch
 from elasticsearch.helpers import streaming_bulk, scan
 
 # --
 # cli 
 
-# parser = argparse.ArgumentParser()
-# parser.add_argument("--config-path", type=str, action='store', default='../config.json')
-# args = parser.parse_args()
+parser = argparse.ArgumentParser()
+parser.add_argument("--config-path", type=str, action='store', default='../config.json')
+parser.add_argument("--index", type=str, action='store', required=True)
+parser.add_argument("--field-name", type=str, action='store', required=True)
+parser.add_argument("--threshold", type=float, action='store', default=7)
+args = parser.parse_args()
 
 config = json.load(open(args.config_path))
-# config = json.load(open('../config.json'))
 client = Elasticsearch([{
     'host' : config['es']['host'], 
     'port' : config['es']['port']
 }], timeout = 60000)
 
-for a in scan(client, index='ernest_suspensions_cat', query={"_source" : "company"}):
-    company = a['_source']['company']
-    print company
-    res = client.search(index='edgar_index_cat', body={
-        "size" : 1,
+# --
+# Run
+
+def run():
+    query = {
+        "_source" : args.field_name,
         "query" : {
-            "match" : {
-                "name" : company
-            }
-        }    
-    })['hits']['hits']
-    if res:
-        print res[0]['_source']['name']
-    else:
-        print '-- no match --'
-    print
-
-
-
-
-# -- 
-# define query
-
-query = {
-  "query" : {
-      "filtered" : {
-          "filter" : {
-              "missing" : {
-                 "field" : "__meta__.halts"
-              }
-          }
-      }
-  }
-}
-
-
-# -- 
-# functions
-
-def run(query): 
-    for a in scan(client, index=config['symbology']['index'], query=query): 
-        res = client.search(index=config['suspension']['index'], body={
-            "_source" : ["company", "date", "link"],
-            "query" : {
-                "match" : {
-                    "company" : a['_source']['name']
-                    }
-                }
-            })
-        
-        if res['hits']['total'] > 0:
-          mtc       = res['hits']['hits'][0]['_source']
-          sym_name  = a['_source']['name'].lower()
-          halt_name = mtc['company'].lower() 
-          x         = fuzz.token_sort_ratio(sym_name, halt_name)
-          y         = fuzz.ratio(sym_name, halt_name)
-          halts     = {"match_attempted" : True}
-          if res['hits']['hits'][0]['_score'] >= 1 and x >= 90):
-              halts.update(mtc)
-              halts.update({
-                  "fuzz_ratio"            : y,
-                  "fuzz_token_sort_ratio" : x, 
-                  "match_score"           : a['_score']
-              })
-
-        yield {
-            "_id"      : a['_id'],
-            "_type"    : config['symbology']['_type'],
-            "_index"   : config['symbology']['index'],
-            "_op_type" : "update",
-            "doc" : {
-                "__meta__" : {
-                    "halts" : halts
+            "filtered" : {
+                "filter" : {
+                    "and" : [
+                        {
+                            "missing" : {
+                                "field" : "__meta__.sym.match_attempted"
+                            }
+                        },
+                        {
+                            "exists" : {
+                                "field" : args.field_name
+                            }
+                        }
+                    ]
                 }
             }
         }
+    }
+    total_count = client.count(index=config[args.index]['index'], body=query)['count']
+    
+    counter = 0
+    for a in scan(client, index=config[args.index]['index'], query=query):
+        sym = {"match_attempted" : True}
+        
+        res = client.search(index=config['symbology']['index'], body={
+            "size"    : 1,
+            "_source" : "cik",
+            "query"   : {
+                "match_phrase" : {
+                    "name" : a['_source'][args.field_name]
+                }
+            }    
+        })['hits']['hits']
+        
+        if res:
+            if res[0]['_score'] > args.threshold:
+                sym.update(res[0]['_source'])
+            
+        yield {
+            "_index"   : a['_index'],
+            "_type"    : a['_type'],
+            "_id"      : a['_id'],
+            "_op_type" : "update",
+            "doc" : {
+                "__meta__" : {
+                    "sym" : sym     
+                }
+            }            
+        }
+        counter += 1
+        sys.stdout.write('\r Completed \t %d \t out of \t %d' % (counter, total_count))
+        sys.stdout.flush()
 
-for a,b in streaming_bulk(client, run(query), chunk_size=1000, raise_on_error=False):
-    print a, b
+
+if __name__ == "__main__":
+    for a,b in streaming_bulk(client, run(), chunk_size=100, raise_on_error=False):
+        pass
+    print '\ndone\n'
