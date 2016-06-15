12f17171531a1091f0054abec92076c724d8317d
Emmett-Culhane <culhane@SP2-16-242-ML.local>
2016-06-15 16:49:45 -0400

    adding timeout

---
+++ scrape/dev-xbrl-parse.R
@@ -0,0 +1,80 @@
+require(R.utils)
+library(Rcpp)
+library(rvest)
+library(XML)
+library(XBRL)
+options(stringsAsFactors = TRUE)
+
+
+args   <- commandArgs(trailingOnly = TRUE)
+
+newdir <- file.path('/home/ubuntu/sec/', args[1], args[2], sep = '') 
+
+zippedFiles  <-list.files(newdir)
+finalDir     <-file.path(paste('/home/ubuntu/sec/parsed_min__', args[1], '__', args[2], sep=''))
+print(finalDir)
+dir.create(finalDir, showWarnings = FALSE) 
+
+
+
+buildFrame <- function(name) {
+        x                   <- name
+        name                <- as.data.frame(xbrl.vars[name])
+        colnames(name)      <- c(gsub(paste('^', x, '.', sep = ""), '', colnames(name)))
+        return(name)
+}
+
+
+parseDoc <- function(u) {
+    tryCatch({
+            for(m in list.files('/home/ubuntu/sec/unzipped')){
+                if(length(grep(pattern="[[:digit:]].xml", x=m))==1) { 
+                    print(m) 
+                    inst      <- file.path('/home/ubuntu/sec/unzipped', m)
+                    xbrl.vars <- xbrlDoAll(inst, verbose=FALSE)
+                    
+                    # build frames
+                    fact    <- buildFrame('fact')
+                    context <- buildFrame('context')
+                    # joins tables to fact 
+                    join1   <- merge(x = fact, y = context, by = "contextId", all.x = TRUE)
+                    # write out file          
+                    title   <-gsub("-|.xml", "", m)  
+                    print(title)
+                    loc    <- file.path(finalDir,paste0(title,'.csv'))
+                    print(loc) 
+                    write.table(join1, file = loc, sep = "," , append = TRUE)    
+                    unlink("/home/ubuntu/sec/unzipped/*")
+                    unlink(file.path(newdir, u))
+                }
+            }
+        }, 
+        error = function(e) {unlink("/home/ubuntu/sec/unzipped/*")}
+        )
+}
+
+
+for(u in zippedFiles){
+    unzip(file.path(newdir, u), list=FALSE, overwrite=TRUE, junkpaths=FALSE, exdir='/home/ubuntu/sec/unzipped',
+             unzip = "internal", setTimes=FALSE)
+    tryCatch(
+        expr = {
+            evalWithTimeout(
+                {parseDoc(u)}, 
+                    timeout = 300)
+            },
+        TimeoutException = function(ex) cat("Timeout. Skipping.")
+    )
+}
+
+
+unlink("/home/ubuntu/sec/unzipped/*")
+
+
+
+
+
+
+
+
+
