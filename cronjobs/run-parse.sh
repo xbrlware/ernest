e35a8af4149001d0db54357d978c446cdbee4472
Emmett-Culhane <culhane@SP2-16-242-ML.hsd1.pa.comcast.net>
2016-06-20 17:32:50 -0400

    increasing timeout param in parsing shell

--- cronjobs/run-parse.sh
+++ cronjobs/run-parse.sh
@@ -5,5 +5,5 @@ cd /home/ubuntu/sec/filings
 while ls | grep -q .
 do  
   echo 'parsing'
-  timeout 300s Rscript /home/ubuntu/ernest/scrape/dev-xbrl-parse.R $1 $2 
+  timeout 600s Rscript /home/ubuntu/ernest/scrape/dev-xbrl-parse.R $1 $2 
 done
