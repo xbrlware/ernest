e115e35ef97af13ec0858b3f8b4ba1ea746d07f8
Emmett-Culhane <culhane@SP2-16-242-ML.local>
2016-06-22 13:44:09 -0400

    fixing syntax issue

--- scrape/xbrl-download.py
+++ scrape/xbrl-download.py
@@ -92,7 +92,7 @@ def SECdownload( year, month ):
     itemIndex = 0
     edgarFilingsFeed = 'http://www.sec.gov/Archives/edgar/monthly/xbrlrss-' + str(year) + '-' + str(month).zfill(2) + '.xml'
     print( edgarFilingsFeed )
-    if not os.path.exists( "/home/ubuntu/sec/filings__" + str(year) + "__" + str(month))
+    if not os.path.exists("/home/ubuntu/sec/filings__" + str(year) + "__" + str(month)):
         os.makedirs( "/home/ubuntu/sec/filings__" + str(year) + "__" + str(month))
     target_dir = "/home/ubuntu/sec/filings__" + str(year) + "__" + str(month) + "/"
     try:
