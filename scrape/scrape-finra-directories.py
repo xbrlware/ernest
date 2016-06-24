58edbcc02a7d10c55ca9258e409e6b880cf93a81
Emmett-Culhane <culhane@SP2-16-242-ML.local>
2016-06-24 12:44:28 -0400

    resolving conflicts

--- scrape/scrape-finra-directories.py
+++ scrape/scrape-finra-directories.py
@@ -1,53 +1,26 @@
 import json
-import itertools
 import math
 import argparse
 import urllib2
-from urllib2 import urlopen
 
 from elasticsearch import Elasticsearch
-from elasticsearch.helpers import scan, streaming_bulk
 
 # -- 
 # cli
 
 parser = argparse.ArgumentParser(description='ingest_finra_docs')
-parser.add_argument("--directory", type = str, action = 'store')
+parser.add_argument("--directory", type=str, action='store')
 parser.add_argument("--config-path", type=str, action='store', default='../config.json')
 args = parser.parse_args()
 
+config = json.load(open(args.config_path))
+client = Elasticsearch([{'host' : config['es']['host'], 'port' : config['es']['port']}])
 
-# -- 
-# config
-
-config_path = args.config_path
-config      = json.load(open(config_path))
-
-
-# --
-# set vars 
-
-if args.directory == 'otc': 
-    url = 'http://otce.finra.org/Directories/DirectoriesJson?pgnum='
-    INDEX  = config['otc_directory']['index']
-    TYPE   = config['otc_directory']['_type']
-# elif args.directory == 'halts':
-#     url = 'http://otce.finra.org/TradeHaltsHistorical/TradeHaltsHistoricalJson?pgnum='
-#     INDEX  = config['otc_halts']['index']
-#     TYPE   = config['otc_halts']['_type']
-elif args.directory == 'delinquency':
-    url = 'http://otce.finra.org/DCList/DCListJson?pgnum='
-    INDEX  = config['otc_delinquency']['index']
-    TYPE   = config['otc_delinquency']['_type']
-else: 
-    print('-- must choose directory: otc, halts, delinuency --')
-    raise
-
-
-# --
-# es connection
-
-client   = Elasticsearch([{'host' : config['es']['host'], 'port' : config['es']['port']}])
+urls = {
+    "directory"   : 'http://otce.finra.org/Directories/DirectoriesJson?pgnum=',
+    "halts"       : 'http://otce.finra.org/TradeHaltsHistorical/TradeHaltsHistoricalJson?pgnum=',
+    "delinquency" : 'http://otce.finra.org/DCList/DCListJson?pgnum=',
+}
 
 # -- 
 # functions
@@ -57,17 +30,16 @@ def ingest_directory(url, INDEX, TYPE):
     r = x['iTotalRecords']
     n = int(math.ceil(float(r) / 25))
     for i in range(0, n + 1): 
-        x    = json.load(urllib2.urlopen(url + str(i)))
-        out  = x['aaData']
-        for x in out:
-            # if args.directory == 'halts': 
-            #     _id = str(x['TradeHaltID']) + '_' + str(x['SecurityID'])
-            # else: 
-            _id = str(x['SecurityID'])
-            client.index(index=INDEX, doc_type=TYPE, body=x, id=_id) 
-
+        x   = json.load(urllib2.urlopen(url + str(i)))
+        out = x['aaData']
+        for i in out:
+            client.index(index=INDEX, doc_type=TYPE, body=i, id=i['SecurityID']) 
 
 # --
 # run
 
-ingest_directory(url, INDEX, TYPE)
+ingest_directory(
+    urls[args.directory], 
+    config['otc_%s' % args.directory]['index'], 
+    config['otc_%s' % args.directory]['_type']
+)
