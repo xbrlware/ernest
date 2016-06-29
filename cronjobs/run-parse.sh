345508926de4da4fd62bb3dc4d022f5104b172f1
Emmett-Culhane <culhane@SP2-16-242-ML.local>
2016-06-29 11:15:46 -0400

    updating shell scripts

--- cronjobs/run-parse.sh
+++ cronjobs/run-parse.sh
@@ -7,3 +7,11 @@ do
   echo 'parsing'
   timeout 600s Rscript /home/ubuntu/ernest/scrape/xbrl-parse.R $1 $2; python /home/ubuntu/ernest/scrape/xbrl-ingest.py --year=$1 --month=$2
 done
+
+cd /home/ubuntu
+
+rm -r /home/ubuntu/sec/filings__$1__$2
+rm -r /home/ubuntu/sec/parsed_min__$1__$2
+rm -r /home/ubuntu/sec/unzipped__$1__$2
+
+
