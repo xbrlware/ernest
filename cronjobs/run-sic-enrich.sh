fd6fa02dee6dc08d1749e08963bdcb6e12651c31
Emmett-Culhane <culhane@SP2-16-242-ML.hsd1.pa.comcast.net>
2016-07-07 15:16:55 -0400

    adding correct script to shell

--- cronjobs/run-sic-enrich.sh
+++ cronjobs/run-sic-enrich.sh
@@ -3,7 +3,7 @@
 echo 'run-sic-enrich.sh'
 
 echo 'adding sic info to symbology documents...'
-python ../enrich/enrich-sic.py --index='symbology'
+python ../enrich/enrich-add-sic-descs.py --index='symbology'
 
 echo 'adding sic info to ownership documents...'
-python ../enrich/enrich-sic.py --index='ownership'
+python ../enrich/enrich-add-sic-descs.py --index='ownership'
