6c1cf568568c9804b6924737a5aa9055c6c79f44
Emmett-Culhane <culhane@SP2-16-242-ML.hsd1.pa.comcast.net>
2016-07-07 12:10:13 -0400

    fixing finra scrape infrastructure

--- cronjobs/run-finra-scrapes.sh
+++ cronjobs/run-finra-scrapes.sh
@@ -5,8 +5,5 @@ echo "run-finra-scrapes.sh"
 echo "directory"
 python ../scrape/scrape-finra-directories.py --directory='directory'
 
-echo "halts"
-python ../scrape/scrape-finra-directories.py --directory='halts'
-
 echo "delinquency"
 python ../scrape/scrape-finra-directories.py --directory='delinquency'
