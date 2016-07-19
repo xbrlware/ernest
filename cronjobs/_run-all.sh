2e04cc4156dba9a017abf622cf8d85c05064c73a
Emmett-Culhane <culhane@SP2-16-242-ML.local>
2016-07-19 13:45:29 -0400

    reordering shell scripts in run all

--- cronjobs/_run-all.sh
+++ cronjobs/_run-all.sh
@@ -22,11 +22,13 @@ echo "-- enrich otc, symbology & ownership --"
 cd $ERNEST_PATH/cronjobs/ && bash run-add-otc-flag.sh
 cd $ERNEST_PATH/cronjobs/ && bash run-sic-enrich.sh
 
+echo "-- get new xbrl sub docs if available --" 
+cd $ERNEST_PATH/cronjobs/ && bash run-xbrl-submission-scrape.sh
+
 echo "-- update delinquency --"
 cd $ERNEST_PATH/cronjobs/ && bash run-build-delinquency.sh
 
-echo "-- get new xbrl sub docs if available --" 
-cd $ERNEST_PATH/cronjobs/ && bash run-xbrl-submission-scrape.sh
+echo "-- get new xbrl rss docs --" 
 cd $ERNEST_PATH/cronjobs/ && bash run-xbrl.sh
 
 echo "-- compute delinquency --"
