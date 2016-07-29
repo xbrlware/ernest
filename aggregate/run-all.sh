55ec0c29c35ed120c0f2122ca56298784478b05e
ben <ben@gophronesis.com>
2016-07-29 03:02:04 +0000

    cleaning up run-all.sh

--- aggregate/run-all.sh
+++ aggregate/run-all.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
 
-CONFIG_PATH=../config.json
+# run-all.sh
+# Run all aggregations
+# 
+# Ex:
+#   ./run-all.sh ../config.json
+
+echo 'run-all (aggregations)'
+
+CONFIG_PATH$1
 SPARK_CMD="spark-submit --master "local[*]" --jars $SPARK_HOME/jars/elasticsearch-hadoop-2.3.0.jar"
 
-# echo '-- setting mapping --'
-# ... Do we need to set the mapping? ...
-
+echo '\t searchterms'
 $SPARK_CMD aggregate-searchterms.py --config-path $CONFIG_PATH
+
+echo '\t symbology'
 $SPARK_CMD aggregate-symbology.py   --config-path $CONFIG_PATH
+
+echo '\t delinquency'
 $SPARK_CMD aggregate-delinquency.py --config-path $CONFIG_PATH
+
+echo '\t financials'
+$SPARK_CMD aggregate-financials.py --config-path $CONFIG_PATH
+
+echo '\t 2-hop-otc'
 $SPARK_CMD aggregate-2-hop-otc.py --config-path $CONFIG_PATH
+
+echo '\t suspensions'
 $SPARK_CMD aggregate-suspensions.py --config-path $CONFIG_PATH
+
+echo '\t crowdsar'
 $SPARK_CMD aggregate-crowdsar.py --config-path $CONFIG_PATH
