2d73cc1c66f44e826c4c34b0de3568067fde9855
Morgan Peterson <morgan@gophronesis.com>
2017-04-25 06:41:32 -0400

    inital attempt at fixing aggregations

--- aggregate/run-all.sh
+++ aggregate/run-all.sh
@@ -6,32 +6,20 @@
 # Ex:
 #   ./run-all.sh ../config.json
 
-echo 'run-all (aggregations)'
-
-
 CONFIG_PATH=$1
-SPARK_CMD="spark-submit --master "local[*]" --jars $SPARK_HOME/jars/elasticsearch-hadoop-2.3.0.jar"
 
-echo '\t setting mappings'
-python ./set-mappings.py --config-path $CONFIG_PATH
+python2.7 set-mappings.py --config-path $CONFIG_PATH
 
-echo '\t searchterms'
-$SPARK_CMD aggregate-searchterms.py --config-path $CONFIG_PATH
+python2.7 aggregate-searchterms.py --config-path $CONFIG_PATH
 
-echo '\t symbology'
-$SPARK_CMD aggregate-symbology.py   --config-path $CONFIG_PATH
+# $SPARK_CMD aggregate-symbology.py   --config-path $CONFIG_PATH
 
-echo '\t delinquency'
-$SPARK_CMD aggregate-delinquency.py --config-path $CONFIG_PATH
+# $SPARK_CMD aggregate-delinquency.py --config-path $CONFIG_PATH
 
-echo '\t financials'
-$SPARK_CMD aggregate-financials.py --config-path $CONFIG_PATH
+# $SPARK_CMD aggregate-financials.py --config-path $CONFIG_PATH
 
-echo '\t 2-hop-otc'
-$SPARK_CMD aggregate-2-hop-otc.py --config-path $CONFIG_PATH
+# $SPARK_CMD aggregate-2-hop-otc.py --config-path $CONFIG_PATH
 
-echo '\t suspensions'
-$SPARK_CMD aggregate-suspensions.py --config-path $CONFIG_PATH
+# $SPARK_CMD aggregate-suspensions.py --config-path $CONFIG_PATH
 
-echo '\t crowdsar'
-$SPARK_CMD aggregate-crowdsar.py --config-path $CONFIG_PATH
+# $SPARK_CMD aggregate-crowdsar.py --config-path $CONFIG_PATH
