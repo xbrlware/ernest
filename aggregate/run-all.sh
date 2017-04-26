26ae3a402d866681c7c217cff0005b543b40f2a1
Morgan Peterson <morgan@gophronesis.com>
2017-04-26 17:28:40 -0400

    aggregate-2-hop-otc works

--- aggregate/run-all.sh
+++ aggregate/run-all.sh
@@ -8,17 +8,17 @@
 
 CONFIG_PATH=$1
 
-# python2.7 set-mappings.py --config-path $CONFIG_PATH
+python2.7 set-mappings.py --config-path $CONFIG_PATH
 
-# python2.7 aggregate-searchterms.py --config-path $CONFIG_PATH
+python2.7 aggregate-searchterms.py --config-path $CONFIG_PATH
 
-# python2.7 aggregate-symbology.py   --config-path $CONFIG_PATH
+python2.7 aggregate-symbology.py   --config-path $CONFIG_PATH
 
-# python2.7 aggregate-delinquency.py --config-path $CONFIG_PATH
+python2.7 aggregate-delinquency.py --config-path $CONFIG_PATH
 
 python2.7 aggregate-financials.py --config-path $CONFIG_PATH
 
-# $SPARK_CMD aggregate-2-hop-otc.py --config-path $CONFIG_PATH
+python2.7 aggregate-2-hop-otc.py --config-path $CONFIG_PATH
 
 # $SPARK_CMD aggregate-suspensions.py --config-path $CONFIG_PATH
 
