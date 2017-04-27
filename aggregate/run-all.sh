f053de67e623e6bb9e9af2711ad3a2a7cb9a98bc
Morgan Peterson <morgan@gophronesis.com>
2017-04-27 09:04:05 -0400

    aggregate-suspensions is working

--- aggregate/run-all.sh
+++ aggregate/run-all.sh
@@ -20,6 +20,6 @@ python2.7 aggregate-financials.py --config-path $CONFIG_PATH
 
 python2.7 aggregate-2-hop-otc.py --config-path $CONFIG_PATH
 
-# $SPARK_CMD aggregate-suspensions.py --config-path $CONFIG_PATH
+python2.7 aggregate-suspensions.py --config-path $CONFIG_PATH
 
 # $SPARK_CMD aggregate-crowdsar.py --config-path $CONFIG_PATH
